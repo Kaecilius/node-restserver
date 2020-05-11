db63bdaaa590414670430b3548f6aa78ac0feeb3
Alex Gabriel Yovera Loayza (HAY-LIM) <alexy@hayduk.com.pe>
2020-05-10 23:26:22 -0500

    rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,21 @@
 // ===========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ===========================
+// ENTORNO
+// ===========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ===========================
+// BASE DE DATOS
+// ===========================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://adminatlas:aAAr3rjZfzfVJnuI@cluster0-xnhdg.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 require('./config/config');
 
 const express = require('express');
+
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
 
 // parse application/x-www-form-urlencoded
@@ -10,40 +13,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('Get  Usuario')
-})
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err, res) => {
+    if (err) throw err;
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-})
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete  Usuario')
-})
+    console.log('Base de datos ONLINE')
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto 3000');
